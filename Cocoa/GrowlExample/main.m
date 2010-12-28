46eac3b9b02bdf94f5c98870e7e71c2c5c241c67
John Haitas <john@haitas.com>
2010-12-27 22:53:30 -0600

    removing J. Haitas, LLC

--- Cocoa/GrowlExample/main.m
+++ Cocoa/GrowlExample/main.m
@@ -3,7 +3,7 @@
 //  GrowlExample
 //
 //  Created by John Haitas on 12/26/10.
-//  Copyright 2010 J. Haitas, LLC. All rights reserved.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
